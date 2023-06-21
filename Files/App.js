a6cdfbebb6f78769648bcc205b024d57555c4c66
Vishnu Patel <109633281+viptl123@users.noreply.github.com>
2023-06-21 15:42:36 -0500

    Add files via upload

--- Files/App.js
+++ Files/App.js
@@ -9,15 +9,18 @@ import HomeScreen from './Pages/HomeScreen';
 import Register from './Pages/Register';
 
 
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  console.log("App.js")
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen options={{headerShown: false}} name="Login" component ={LoginScreen}/>
-        <Stack.Screen name="Home" component={HomeScreen}/>
-        <Stack.Screen name="Register" component ={Register}/>
+        <Stack.Screen name='Home' component={HomeScreen}/>
+        <Stack.Screen name='Register' component ={Register}/>
+
         
       </Stack.Navigator>
     </NavigationContainer>
@@ -30,5 +33,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
 });
