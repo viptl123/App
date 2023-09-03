a6a574e37fbdb292d68d7ee80e555c5760df27bc
Vishnu Patel <109633281+viptl123@users.noreply.github.com>
2023-09-03 00:04:01 -0500

    Add files via upload

---
+++ WorkoutAppFilesUpdates/Navigation/Routes.js
@@ -0,0 +1,13 @@
+export default Routes = {
+    Home: "Home",
+    Login: "Login",
+    Register: "Register",
+    MuscleGroups: "MuscleGroups",
+    ExerciseGroups: "ExerciseGroups",
+    WorkoutScreen: "WorkoutScreen",
+    ExerciseDisplay: "ExerciseDisplay",
+    CalenderScreen: "CalenderScreen",
+    NameWorkoutScreen: "NameWorkoutScreen",
+
+
+}
